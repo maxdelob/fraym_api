22eaa9060e4ca86f99f5c9d85ca6991ec40102e7
Maxime DELOBELLE <maxime.delobelle@niji.fr>
2021-04-16 01:24:09 +0200

    deploy empty API to heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require( "express" );
+const port = process.env.PORT || 3000;
 const app = express();
-const port = 1418; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
