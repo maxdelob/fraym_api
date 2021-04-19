f6eee8957b5157a5bf248794092f34d6481c6c58
Maxime DELOBELLE <maxime.delobelle@niji.fr>
2021-04-19 15:32:50 +0200

    chart data => add dates end point

--- routes/chart-data.js
+++ routes/chart-data.js
@@ -4,8 +4,7 @@ const connString = require('../config').dbInfo;
 const { Client } = require('pg');
 
 // 'SELECT $1 AS value', 123
-router.get('/', function(req, res, next) { 
-    // const client = new Client({ connectionString: connString, ssl: { rejectUnauthorized: false } });
+router.get('/', function(req, res, next) {  
     const query = "SELECT * from prices where ticker = '" + req.params.id + "'" 
     const client = new Client({ connectionString: connString, ssl: { rejectUnauthorized: false } });
     client.connect();
@@ -15,12 +14,22 @@ router.get('/', function(req, res, next) {
     })
 });
 
+router.get('/dates', function(req, res){
+    const query = "select min(date), max(date) from prices  where ticker = '" + req.params.id + "'" 
+    const client = new Client({ connectionString: connString, ssl: { rejectUnauthorized: false } });
+    client.connect();
+    client.query(query, (err, results) => {
+        if(err) throw (err);
+        res.json(results.rows);
+    })
+})
+
 const parseData = (data) => {
     return data.map(e => { return {name: parseName(e), value : [parseDate(e.date), e.value_close]} })
 }
 
 const parseDate = (date) => {
-    return date.toLocaleString().split(' ')[0]
+    return date.toLocaleString().split(' ')[0].replace('-', '/')
 } 
 const parseName = (data) => {
     return data.date + ' - ' + data.ticker
