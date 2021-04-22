c5c92400a126dad1121ac9fab8dcc52aaf229bf4
Maxime DELOBELLE <maxime.delobelle@niji.fr>
2021-04-22 21:49:46 +0200

    fix companies end point regression

--- routes/companies.js
+++ routes/companies.js
@@ -3,13 +3,21 @@ var router = express.Router();
 const connString = require('../config').dbInfo;
 const { Client } = require('pg');
 
+router.get('/', function(req, res) { 
+    const client = new Client({ connectionString: connString, ssl: { rejectUnauthorized: false } });
+    client.connect();
+    let query = 
+    client.query('SELECT * from companies', (err, results) => {
+        if(err) throw (err);
+        res.json(results.rows);
+        client.end();
+    })
+});
+
 router.get('/:id', function(req, res) { 
     const client = new Client({ connectionString: connString, ssl: { rejectUnauthorized: false } });
     client.connect();
-    let query = 'SELECT * from companies'
-    if(req.params.id){
-        query = query + " WHERE ticker = '" + req.params.id + "'"
-    }
+    let query = "SELECT * from companies WHERE ticker = '" + req.params.id + "'"
     client.query(query, (err, results) => {
         if(err) throw (err);
         res.json(results.rows);
